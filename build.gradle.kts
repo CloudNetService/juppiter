b679a9f8860151b349bdeb31533a1da9650032c5
derklaro <git@derklaro.dev>
2022-07-15 20:13:27 +0200

    build: bump to version 0.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
   id("com.gradle.plugin-publish") version "1.0.0"
 }
 
-version = "0.3.2"
+version = "0.3.3"
 group = "eu.cloudnetservice.gradle"
 
 // we set the kotlin jmvTarget specifically to 1.8 to prevent issues in case the default values changes
