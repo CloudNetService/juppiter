06ee8b4c0c88927bb98cf688b13d0e6ef94d8cc4
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2026-03-15 18:41:25 +0100

    test project for manual testing

---
+++ test/build.gradle.kts
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019-present CloudNetService team & contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+  id("eu.cloudnetservice.juppiter")
+  `java-library`
+  id("net.fabricmc.fabric-loom") version "1.15-SNAPSHOT" apply false
+//  id("net.fabricmc.fabric-loom-remap") apply false
+}
+
+moduleJson {
+  main = "abc"
+}
+
+flavors {
+  registerJavaSourceSets()
+  val common = addFlavor("common")
+  common.dependOn(main)
+  val fabric = addFlavor("fabric/base")
+  fabric.dependOn(common)
+  val forge = addFlavor("forge")
+  forge.dependOn(common)
+  val fabric1211 = addFlavor("fabric/1_21_1")
+  fabric1211.dependOn(fabric)
+
+  val merged = addFlavor("merged")
+  merged.dependOn(forge)
+  merged.dependOn(fabric1211)
+}
+
+tasks.assemble {
+  dependsOn(tasks.named("mergedJar"))
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+//  api("com.google.code.gson:gson:2.13.2")
+  "compileOnlyApi"("com.google.code.gson:gson:2.13.2")
+//  "commonApi"("com.google.code.gson:gson:2.13.2")
+}
